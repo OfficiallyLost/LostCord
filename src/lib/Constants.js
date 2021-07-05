87cb514431b456c14f556c3a45d026cb888c2db4
haseeb <farooqhaseeb49@gmail.com>
2021-07-06 00:14:45 +0100

    opcodes

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -1,4 +1,7 @@
 module.exports.API_VERSION = '9';
-module.exports.WEBSOCKET = `wss://gateway.discord.gg/?v=${this.API_VERSION}&encoding=json&compress=zlib-stream`;
+module.exports.WEBSOCKET = `wss://gateway.discord.gg/?v=${this.API_VERSION}&encoding=json`;
 module.exports.CDN_BASE_URL = 'https://cdn.discordapp.com';
 module.exports.BASE_URL = `https://discord.com/api/v${this.API_VERSION}`;
+module.exports.OPCODES = {
+	HELLO: 10
+};
