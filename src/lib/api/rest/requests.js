828c58211f9f719d82630631275954d36b22c179
haseeb <farooqhaseeb49@gmail.com>
2021-07-06 21:10:04 +0100

    add method & error handle

--- src/lib/api/rest/requests.js
+++ src/lib/api/rest/requests.js
@@ -3,15 +3,25 @@ const endpoints = require('./Endpoints');
 
 class Request {
 	constructor(client) {
-		// this.client = { token: 'NjUwMTM2OTg0MjExMjkyMTgw.XeG9Pw.eJfaduBg1nzk2HkoOi0NKInezO4' };
 		this.RequestManager = new RequestManager(client);
 		this.endpoints = endpoints;
 	}
 
 	async getGuildAuditLogs(guildID) {
+		if (!guildID) return Promise.reject(new Error('You must enter a guild ID to get data on'));
+
 		return await this.RequestManager.request(
 			'GET',
 			`${this.RequestManager.constants.BASE_URL}${this.endpoints.GUILD_AUDIT_LOGS(guildID)}`
+		);
+	}
+
+	async getGuild(guildID) {
+		if (!guildID) return Promise.reject(new Error('You must enter a guild ID to get data on'));
+
+		return await this.RequestManager.request(
+			'GET',
+			`${this.RequestManager.constants.BASE_URL}${this.endpoints.GET_GUILD(guildID)}`
 		);
 	}
 }
