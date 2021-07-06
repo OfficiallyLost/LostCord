1c126cd71ad26b13a9ab2c0988a92c41224df1bf
haseeb <farooqhaseeb49@gmail.com>
2021-07-06 21:03:56 +0100

    create discord class

---
+++ src/lib/discord/Discord.js
@@ -0,0 +1,29 @@
+const request = require('../api/rest/requests');
+
+class Discord {
+	constructor(client) {
+		this.client = client;
+	}
+
+	get guild() {
+		if (!this.client.options.guild) {
+			return Promise.reject(new Error('You must add a guild ID to the client options'));
+		}
+
+		const data = this.client.request.RequestManager
+			.request(
+				'GET',
+				`${this.client.request.RequestManager.constants.BASE_URL}${this.client.request.endpoints.GET_GUILD(
+					this.client.options.guild
+				)}`
+			)
+			.then((e) => e.data);
+
+		data.then((e) => {
+			e.invite = this.client.request.RequestManager.constants.DISCORD.INVITE;
+		});
+		return data;
+	}
+}
+
+module.exports = Discord;
