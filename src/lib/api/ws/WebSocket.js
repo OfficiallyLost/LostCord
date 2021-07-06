6e7ba88816ff711f8bd68a50226f732bcc372752
haseeb <farooqhaseeb49@gmail.com>
2021-07-06 01:05:50 +0100

    (^ i mean receiving data) rename file

--- src/lib/api/ws/WebSocket.js
+++ src/lib/api/ws/WebSocket.js
@@ -4,6 +4,14 @@ class WebSocket {
 	constructor(client) {
 		this.WebSocketManager = new WebSocketManager(client);
 	}
+
+	connect() {
+		this.WebSocketManager.connect();
+	}
+
+	on() {
+		return this.client.on('READY');
+	}
 }
 
 module.exports = WebSocket;
