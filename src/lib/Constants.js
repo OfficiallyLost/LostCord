d1b775e0eaec95285b9ec4956b696986e96f2b0c
haseeb <farooqhaseeb49@gmail.com>
2021-07-06 21:04:34 +0100

    add some constants

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -2,6 +2,10 @@ module.exports.API_VERSION = '9';
 module.exports.WEBSOCKET = `wss://gateway.discord.gg/?v=${this.API_VERSION}&encoding=json`;
 module.exports.CDN_BASE_URL = 'https://cdn.discordapp.com';
 module.exports.BASE_URL = `https://discord.com/api/v${this.API_VERSION}`;
+module.exports.DISCORD = {
+	INVITE: 'https://discord.gg/FWTRPS9'
+};
+
 module.exports.OPCODES = {
 	HELLO: 10
 };
