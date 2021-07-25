43963d109ef52639bbabd0604f9c3d97c39d90fe
haseeb <farooqhaseeb49@gmail.com>
2021-07-25 15:14:03 +0100

    add channel and identify constant

--- src/lib/Constants.js
+++ src/lib/Constants.js
@@ -2,10 +2,23 @@ module.exports.API_VERSION = '9';
 module.exports.WEBSOCKET = `wss://gateway.discord.gg/?v=${this.API_VERSION}&encoding=json`;
 module.exports.CDN_BASE_URL = 'https://cdn.discordapp.com';
 module.exports.BASE_URL = `https://discord.com/api/v${this.API_VERSION}`;
+module.exports.DISCORD_URL = `https://discord.com/`;
 module.exports.DISCORD = {
 	INVITE: 'https://discord.gg/FWTRPS9'
 };
 
 module.exports.OPCODES = {
-	HELLO: 10
+	HELLO: 10,
+	IDENTIFY: 2
+};
+
+module.exports.CHANNEL = {
+	ZERO: 'Guild Text',
+	ONE: 'DM',
+	TWO: 'Guild Voice',
+	THREE: 'Group DM',
+	FOUR: 'Guild Category',
+	FIVE: 'Guild News',
+	SIX: 'Guild Store',
+	THIRTEEN: 'Guild Stage'
 };
