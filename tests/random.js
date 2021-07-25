25fa497b5d848de357de73481787c73edc17208a
haseeb <farooqhaseeb49@gmail.com>
2021-07-25 15:17:52 +0100

    tests

--- tests/random.js
+++ tests/random.js
@@ -1,8 +1,10 @@
 const LostCord = require('../index');
 const client = new LostCord('', {
-	guild: '650128487511883796'
+	guild: ''
 });
 
-client.on('messageCreate', (message) => console.log(message));
+client.on('messageCreate', (message) => {
+	console.log(message);
+});
 
 client.connect();
