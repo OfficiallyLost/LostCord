df45b4cb8e68771db849f9fc107ea425f318e93f
haseeb <farooqhaseeb49@gmail.com>
2021-07-30 03:21:14 +0100

    idk random testing guilds

--- src/lib/discord/DiscordEvents.js
+++ src/lib/discord/DiscordEvents.js
@@ -9,6 +9,13 @@ function handleEvents(data, payload) {
 
 		case 'READY':
 			data.client.user = new User(payload.d.user, data.client);
+			data.client.guilds = payload.d.guilds.map((e) => e.id);
+
+			(async function() {
+				for (const guildID of data.client.guilds) {
+					const guild = await data.client.request.getGuild(guildID);
+				}
+			})();
 
 			data.client.log('[WS] READY');
 			data.client.emit('ready');
