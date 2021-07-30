3c202b88d0e39f974d06525b782454fba7419bfc
haseeb <farooqhaseeb49@gmail.com>
2021-07-30 21:02:05 +0100

    test client#channels

--- tests/random.js
+++ tests/random.js
@@ -2,9 +2,13 @@ const LostCord = require('../index');
 const client = new LostCord('', {
 	guild: ''
 });
-
 client.on('messageCreate', async (message) => {
-	(await message.channel).createMessage({ content: 'i', embeds: [ { title: 'hi' } ] });
+	if (message.raw.author.id !== '') return;
+
+	(await message.channel).createMessage({
+		content: [ `${(await message.channel).type} | ${message.raw.content}` ],
+		embeds: [ { title: 'hi' } ]
+	});
 	console.log(`${message.raw.author.username} said ${message.raw.content} | ${client.user.raw.username}`);
 });
 
