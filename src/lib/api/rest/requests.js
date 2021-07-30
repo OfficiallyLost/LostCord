223f2e0af926a673760ddc02ff0d82a97539fcb0
haseeb <farooqhaseeb49@gmail.com>
2021-07-30 03:20:46 +0100

    fix createMesasge dapi req func

--- src/lib/api/rest/requests.js
+++ src/lib/api/rest/requests.js
@@ -27,9 +27,30 @@ class Request {
 	}
 
 	async getUser(userID) {
+		if (!userID) return Promise.reject(new Error('You must enter a user ID to get data on'));
+
 		return await this.RequestManager.request(
 			'GET',
 			`${this.RequestManager.constants.BASE_URL}${this.endpoints.GET_USER(userID)}`
+		);
+	}
+
+	async getChannel(channelID) {
+		if (!channelID) return Promise.reject(new Error('You must enter a channel ID to get data on'));
+
+		return await this.RequestManager.request(
+			'GET',
+			`${this.RequestManager.constants.BASE_URL}${this.endpoints.GET_CHANNEL(channelID)}`
+		);
+	}
+
+	async createMessage(channelID, content, embeds, file, components, options) {
+		if (!channelID) return Promise.reject(new Error('You must enter a channel ID to send the message to'));
+
+		return await this.RequestManager.request(
+			'POST',
+			`${this.RequestManager.constants.BASE_URL}${this.endpoints.CREATE_MESSAGE(channelID)}`,
+			{ content, embeds, file, components, options }
 		);
 	}
 }
