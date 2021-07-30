71d79d290c48776edfe011aa28e4ecac356a578b
haseeb <farooqhaseeb49@gmail.com>
2021-07-30 03:22:30 +0100

    create channel prop for msg

--- src/lib/structures/Message.js
+++ src/lib/structures/Message.js
@@ -1,15 +1,32 @@
 const Base = require('./Base');
 const constants = require('../Constants');
+const TextChannel = require('./TextChannel');
 
-class Message {
+class Message extends Base {
 	constructor(raw, client) {
-		// super(raw.id);
+		super(raw.id);
 		this.raw = raw;
 		Object.defineProperty(this, 'client', { value: client });
+		Object.defineProperty(this, 'request', { value: client.request });
+		this.channels = new Map();
 	}
 
 	get url() {
 		return `${constants.DISCORD_URL}channels/${this.raw.guild_id}/${this.raw.channel_id}/${this.raw.id}`;
+	}
+
+	get channel() {
+		if (Object.keys(this.client.messageChannel).length === 0) {
+			return this.request.getChannel(this.raw.channel_id).then((data) => {
+				return (this.client.messageChannel = new TextChannel(data.data, this.client));
+			});
+		}
+
+		return Promise.resolve(this.client.messageChannel);
+	}
+
+	static raw() {
+		return this;
 	}
 }
 
