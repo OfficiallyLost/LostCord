c01ef12891e9a835ffcb6bf24a82d12d71c04d31
haseeb <farooqhaseeb49@gmail.com>
2021-07-30 20:58:42 +0100

    getGuildChannels endpoint url

--- src/lib/api/rest/Endpoints.js
+++ src/lib/api/rest/Endpoints.js
@@ -1,4 +1,5 @@
 module.exports.GET_GUILD = (guildID) => `/guilds/${guildID}`;
+module.exports.GET_GUILD_CHANNELS = (guildID) => `/guilds/${guildID}/channels`;
 module.exports.GUILD_AUDIT_LOGS = (guildID) => `/guilds/${guildID}/audit-logs`;
 module.exports.GET_CHANNEL = (channelID) => `/channels/${channelID}`;
 module.exports.CREATE_MESSAGE = (channelID) => `/channels/${channelID}/messages`;
