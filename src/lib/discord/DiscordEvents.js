c033dd5e0a5247ab72504229d477df96f73c2db8
haseeb <farooqhaseeb49@gmail.com>
2021-07-31 17:20:09 +0100

    data.client.channels optimisation (broke)

--- src/lib/discord/DiscordEvents.js
+++ src/lib/discord/DiscordEvents.js
@@ -1,7 +1,7 @@
 const Message = require('../structures/Message');
 const User = require('../structures/User');
 
-function handleEvents(data, payload) {
+async function handleEvents(data, payload) {
 	switch (payload.t) {
 		case 'MESSAGE_CREATE':
 			data.client.emit('messageCreate', new Message(payload.d, data.client));
@@ -10,22 +10,26 @@ function handleEvents(data, payload) {
 		case 'READY':
 			data.client.user = new User(payload.d.user, data.client);
 			data.client.guilds = payload.d.guilds.map((guild) => guild.id);
-			data.client.users = [];
 			data.client.channels = [];
+			// let guildChannels = [];
 
-			(async function() {
-				let guildChannels = [];
-				for (const guild of data.client.guilds) {
-					guildChannels.push(await data.client.request.getGuildChannels(guild));
-				}
+			// for (const guild of data.client.guilds) {
+			// 	guildChannels.push(await data.client.request.getGuildChannels(guild));
+			// }
 
-				for (const channel of guildChannels[0].data) {
-					data.client.channels.push(channel.id);
-				}
-			})();
+			// for (const channel of guildChannels[0].data) {
+			// 	data.client.channels.push(channel.id);
+			// }
 
 			data.client.log('[WS] READY');
 			data.client.emit('ready');
+
+		case 'GUILD_CREATE':
+			payload.d.channels.map((channel) => data.client.channels.push(channel.id));
+		// for (const channel of payload.d.channels) {
+		// 	console.log(payload.d.channels);
+		// 	data.client.channels.push(channel.id);
+		// }
 	}
 }
 
