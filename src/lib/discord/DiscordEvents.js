17ca0838d9a70497f5c95a657ee3a60f4e9998cc
haseeb <farooqhaseeb49@gmail.com>
2021-07-31 18:39:10 +0100

    emit hello, guild create & handle reconnect event

--- src/lib/discord/DiscordEvents.js
+++ src/lib/discord/DiscordEvents.js
@@ -3,33 +3,36 @@ const User = require('../structures/User');
 
 async function handleEvents(data, payload) {
 	switch (payload.t) {
-		case 'MESSAGE_CREATE':
-			data.client.emit('messageCreate', new Message(payload.d, data.client));
+		case 'HELLO':
+			data.client.emit('hello', payload.d);
 			break;
 
 		case 'READY':
 			data.client.user = new User(payload.d.user, data.client);
 			data.client.guilds = payload.d.guilds.map((guild) => guild.id);
 			data.client.channels = [];
-			// let guildChannels = [];
-
-			// for (const guild of data.client.guilds) {
-			// 	guildChannels.push(await data.client.request.getGuildChannels(guild));
-			// }
-
-			// for (const channel of guildChannels[0].data) {
-			// 	data.client.channels.push(channel.id);
-			// }
+			data.client.users = [];
 
 			data.client.log('[WS] READY');
 			data.client.emit('ready');
+			break;
+
+		case 'RECONNECT':
+			if (data.client.options.reconnectAttempts !== 0) {
+				setInterval(() => {
+					data.ws.connect();
+				}, data.client.options.reconnectAttempts);
+			}
+
+		case 'MESSAGE_CREATE':
+			data.client.emit('messageCreate', new Message(payload.d, data.client));
+			break;
 
 		case 'GUILD_CREATE':
 			payload.d.channels.map((channel) => data.client.channels.push(channel.id));
-		// for (const channel of payload.d.channels) {
-		// 	console.log(payload.d.channels);
-		// 	data.client.channels.push(channel.id);
-		// }
+
+			data.client.emit('guildCreate', payload.d);
+			break;
 	}
 }
 
