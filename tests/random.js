aaf54300bc79cc7958b1a7eec4c70c988ccc21bc
haseeb <farooqhaseeb49@gmail.com>
2021-08-02 13:36:19 +0100

    testing loads of stuff (message#author, client#user, slash cmds

--- tests/random.js
+++ tests/random.js
@@ -3,9 +3,14 @@ const client = new LostCord('', {
 	guild: ''
 });
 
+client.on('reconnect', (message) => {
+	console.log(message);
+});
+
 client.on('messageCreate', async (message) => {
-	if (message.raw.author.id !== '') return;
-	console.log(client);
+	if (message.raw.guild_id !== '850049610776576000') return;
+	await client.createCommand();
+
 	if (message.raw.content.startsWith('e')) {
 		try {
 			const msg = message.raw.content.slice(2);
@@ -21,12 +26,8 @@ client.on('messageCreate', async (message) => {
 			return (await message.channel).createMessage({ content: `\`\`\`js\n${e}\`\`\`` });
 		}
 	}
-	(await message.channel).createMessage({
-		content: `${(await message.channel).type} | ${message.raw.content} | ${(await message.channel)
-			.mention}\nInvite me! <${client.invite}>`,
-		embeds: [ { title: 'hi' } ]
-	});
-	console.log(`${message.raw.author.username} said ${message.raw.content} | ${client.user.raw.username}`);
+	// (await message.channel).createMessage({ content: 'e message' });
+	console.log(`${message.raw.author.username} said ${message.raw.content} | ${client.user.username}`);
 });
 
 client.connect();
