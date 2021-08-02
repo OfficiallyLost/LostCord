be04eca674c8ce18823df107ff7dae9e897dea14
haseeb <farooqhaseeb49@gmail.com>
2021-08-02 13:34:27 +0100

    sort out APPLICATION_COMMAND_CREATE t payload

--- src/lib/discord/DiscordEvents.js
+++ src/lib/discord/DiscordEvents.js
@@ -1,5 +1,6 @@
 const Message = require('../structures/Message');
 const User = require('../structures/User');
+const constants = require('../Constants');
 
 async function handleEvents(data, payload) {
 	switch (payload.t) {
@@ -12,17 +13,29 @@ async function handleEvents(data, payload) {
 			data.client.guilds = payload.d.guilds.map((guild) => guild.id);
 			data.client.channels = [];
 			data.client.users = [];
+			data.client.application = payload.d.application;
 
 			data.client.log('[WS] READY');
 			data.client.emit('ready');
+			data.client.status = 'ready';
+
 			break;
 
 		case 'RECONNECT':
-			if (data.client.options.reconnectAttempts !== 0) {
-				setInterval(() => {
-					data.ws.connect();
-				}, data.client.options.reconnectAttempts);
-			}
+			data.ws.send({ op: constants.OPCODES.RECONNECT, d: null });
+			data.client.emit('reconnect', 'Discord made us reconnect to their servers');
+			break;
+
+		case 'INVALID_SESSION':
+			data.client.ws.disconnect();
+			data.client.ws.connect();
+
+			data.client.emit('invalidSession', payload.d);
+			break;
+
+		case 'APPLICATION_COMMAND_CREATE':
+			console.log(paylaod.d);
+			data.client.emit('slashCommandCreated', payload.d);
 
 		case 'MESSAGE_CREATE':
 			data.client.emit('messageCreate', new Message(payload.d, data.client));
