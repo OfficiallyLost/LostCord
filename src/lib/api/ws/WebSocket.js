6395f0ea4ef1ef2215b69aa0ddc729bde737c959
haseeb <farooqhaseeb49@gmail.com>
2021-08-02 13:33:16 +0100

    client#disconnect (forgot abt this file)

--- src/lib/api/ws/WebSocket.js
+++ src/lib/api/ws/WebSocket.js
@@ -3,10 +3,20 @@ const WebSocketManager = require('./WebSocketManager');
 class WebSocket {
 	constructor(client) {
 		this.WebSocketManager = new WebSocketManager(client);
+		Object.defineProperty(this, 'client', { value: client });
 	}
 
 	connect() {
 		this.WebSocketManager.connect();
+	}
+
+	disconnect() {
+		this.WebSocketManager.disconnect();
+	}
+
+	reset() {
+		this.client.reconnectAttempts = 0;
+		this.client.status = 'disconnected';
 	}
 }
 
