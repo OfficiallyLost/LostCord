efe597e6226f227ea580c858b3d378f8f524c0ab
haseeb <farooqhaseeb49@gmail.com>
2021-08-02 13:35:26 +0100

    clean up user class

--- src/lib/structures/User.js
+++ src/lib/structures/User.js
@@ -3,8 +3,30 @@ const Base = require('./Base');
 class User extends Base {
 	constructor(raw, client) {
 		super(raw.id);
-		this.raw = raw;
+		this.username = raw.username;
+		this.discriminator = raw.discriminator;
+		this.avatar = raw.avatar;
+		this.bot = !!raw.bot;
+		this.public_flags = raw.public_flags;
+		this.system = !!raw.system;
+		this.mfaEnabled = !!raw.mfa_enabled;
 		Object.defineProperty(this, 'client', { value: client });
+	}
+
+	get avatarURL() {
+		if (!this.avatar) {
+			return this.defaultAvatar();
+		};
+		
+		return this.client.requests.avatarURL(this._id, this.avatar);
+	}
+
+	get mention() {
+		return `<@${this._id}>`;
+	}
+
+	get defaultAvatar() {
+		return this.discriminator % 5;
 	}
 }
 
