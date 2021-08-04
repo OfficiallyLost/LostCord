27cfdb48bbf0c881465a4a9b4187d58a98b6c177
haseeb <farooqhaseeb49@gmail.com>
2021-08-05 00:43:59 +0100

    actual discord events now, remove stuff

--- src/lib/discord/DiscordEvents.js
+++ src/lib/discord/DiscordEvents.js
@@ -4,10 +4,6 @@ const constants = require('../Constants');
 
 async function handleEvents(data, payload) {
 	switch (payload.t) {
-		case 'HELLO':
-			data.client.emit('hello', payload.d);
-			break;
-
 		case 'READY':
 			data.client.user = new User(payload.d.user, data.client);
 			data.client.guilds = payload.d.guilds.map((guild) => guild.id);
@@ -18,24 +14,11 @@ async function handleEvents(data, payload) {
 			data.client.log('[WS] READY');
 			data.client.emit('ready');
 			data.client.status = 'ready';
-
-			break;
-
-		case 'RECONNECT':
-			data.ws.send({ op: constants.OPCODES.RECONNECT, d: null });
-			data.client.emit('reconnect', 'Discord made us reconnect to their servers');
-			break;
-
-		case 'INVALID_SESSION':
-			data.client.ws.disconnect();
-			data.client.ws.connect();
-
-			data.client.emit('invalidSession', payload.d);
 			break;
 
 		case 'APPLICATION_COMMAND_CREATE':
-			console.log(paylaod.d);
-			data.client.emit('slashCommandCreated', payload.d);
+			console.log(payload.d);
+			data.client.emit('applicationCommandCreate', payload.d);
 
 		case 'MESSAGE_CREATE':
 			data.client.emit('messageCreate', new Message(payload.d, data.client));
