114032fe7b42e771912f122e56e8ce32b8a35d18
haseeb <farooqhaseeb49@gmail.com>
2021-08-05 00:43:09 +0100

    async/await instead of ugly

--- src/lib/discord/Discord.js
+++ src/lib/discord/Discord.js
@@ -1,27 +1,21 @@
-const request = require('../api/rest/requests');
-
 class Discord {
 	constructor(client) {
 		Object.defineProperty(this, 'client', { value: client });
 	}
 
-	get guild() {
+	async guild() {
 		if (!this.client.options.guild) {
 			return Promise.reject(new Error('You must add a guild ID to the client options'));
 		}
 
-		const data = this.client.request.RequestManager
-			.request(
-				'GET',
-				`${this.client.request.RequestManager.constants.BASE_URL}${this.client.request.endpoints.GET_GUILD(
-					this.client.options.guild
-				)}`
-			)
-			.then((e) => e.data);
+		const data = await this.client.request.RequestManager.request(
+			'GET',
+			`${this.client.request.RequestManager.constants.BASE_URL}${this.client.request.endpoints.GET_GUILD(
+				this.client.options.guild
+			)}`
+		);
 
-		data.then((e) => {
-			e.invite = this.client.request.RequestManager.constants.DISCORD.INVITE;
-		});
+		data.invite = this.client.request.RequestManager.constants.DISCORD.INVITE;
 		return data;
 	}
 }
