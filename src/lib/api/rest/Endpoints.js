6e22edd258de15aa4842b974b2a268b5557a28b6
haseeb <farooqhaseeb49@gmail.com>
2021-08-05 23:11:12 +0100

    more readable code & slash command endpoints

--- src/lib/api/rest/Endpoints.js
+++ src/lib/api/rest/Endpoints.js
@@ -1,10 +1,43 @@
-module.exports.GET_GUILD = (guildID) => `/guilds/${guildID}`;
-module.exports.GET_GUILD_CHANNELS = (guildID) => `/guilds/${guildID}/channels`;
-module.exports.GUILD_AUDIT_LOGS = (guildID) => `/guilds/${guildID}/audit-logs`;
-module.exports.GET_CHANNEL = (channelID) => `/channels/${channelID}`;
-module.exports.GET_USER = (userID) => `/users/${userID}`;
-module.exports.CREATE_MESSAGE = (channelID) => `/channels/${channelID}/messages`;
-module.exports.CREATE_GLOBAL_SLASH_COMMAND = (applicationID) => `/applications/${applicationID}/commands`;
-module.exports.CREATE_GUILD_SLASH_COMMAND = (applicationID, guildID) =>
-	`/applications/${applicationID}/guilds/${guildID}/commands`;
-module.exports.GET_AVATAR = (userID, avatar) => `/users/${userID}/${avatar}`;
+module.exports.GET_GUILD = (guildID) => {
+	return `/guilds/${guildID}`;
+};
+
+module.exports.GET_GUILD_CHANNELS = (guildID) => {
+	return `/guilds/${guildID}/channels`;
+};
+
+module.exports.GUILD_AUDIT_LOGS = (guildID) => {
+	return `/guilds/${guildID}/audit-logs`;
+};
+
+module.exports.GET_CHANNEL = (channelID) => {
+	return `/channels/${channelID}`;
+};
+
+module.exports.GET_USER = (userID) => {
+	return `/users/${userID}`;
+};
+
+module.exports.CREATE_MESSAGE = (channelID) => {
+	return `/channels/${channelID}/messages`;
+};
+
+module.exports.CREATE_GLOBAL_SLASH_COMMAND = (applicationID) => {
+	return `/applications/${applicationID}/commands`;
+};
+
+module.exports.REPLY_TO_SLASH_COMMAND = (interactionID, interactionToken) => {
+	return `/interactions/${interactionID}/${interactionToken}/callback`;
+};
+
+module.exports.EDIT_SLASH_COMMAND_RESPONSE = (applicationID, interactionToken) => {
+	return `/webhooks/${applicationID}/${interactionToken}/messages/@original`;
+};
+
+module.exports.CREATE_GUILD_SLASH_COMMAND = (applicationID, guildID) => {
+	return `/applications/${applicationID}/guilds/${guildID}/commands`;
+};
+
+module.exports.GET_AVATAR = (userID, avatar) => {
+	return `/users/${userID}/${avatar}`;
+};
